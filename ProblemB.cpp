6e7f849093872e0ce301c85da59becb7b9e190c7
Smile <CPSsmile@gmail.com>
2019-03-22 22:16:28 +0800

    ProblemB comments added

--- ProblemB.cpp
+++ ProblemB.cpp
@@ -1,4 +1,4 @@
-/*input
+/* Input Case 1
 -20
 5
 3 2 2 1 1
@@ -21,48 +21,56 @@
 3 10 4 0 0.5 0.4 0.1
 */
 
-/*input
+/* Input Case 2
 -5
 3
 4 3 2
+
 2 3 1 0.6 0.4
 2 4 2 0.3 0.7
-2 1 0 0.5 0.5
 
+2 1 0 0.5 0.5
 2 2 0 0.5 0.5
+
 2 3 0 0.7 0.3
 2 1 0 0.6 0.4
 
 3 10 4 0 0.5 0.4 0.1
 */
 
-/*input
+/* Input Case 3
 -5
 2
 6 4
+
 3 6 4 2 0.4 0.4 0.2
 2 5 3 0.6 0.4
+
 3 4 2 0 0.3 0.3 0.4
 2 2 0 0.5 0.5
+
 2 4 0 0.5 0.5
 */
 
-/*
+/* Input Hint
 H
 number_of_factory
 Weight[1] Weight[2] ...
+
 number_of_g_of_factory[1] g[1][0] g[1][1] g[1][2] ... q[1][0] q[1][1] q[1][2] ...
-number_of_g_of_factory[2] g[2][0] g[2][1] g[2][2] ... q[2][0] q[2][1] q[2][2] ...
-...
-number_of_d_of_factory[1] d[1][0] d[1][1] ... s[1][0] s[1][1] ...
+number_of_d_of_factory[1] d[1][0] d[1][1] d[1][2] ... s[1][0] s[1][1] s[1][2] ...
+
+number_of_g_of_factory[2] g[2][0] g[2][1] ... q[2][0] q[2][1] ...
 number_of_d_of_factory[2] d[2][0] d[2][1] ... s[2][0] s[2][1] ...
+
 ...
+
 number_of_c c[1] c[2] ... beta[1] beta[2] ...
  */
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN=7;
+const int MAXN=20;
 const int MAXM=7;
 const int INF=2139062143;
 const double EPS=1e-12;
@@ -96,6 +104,9 @@ struct FactoryPartGQ
         for (int i = 0; i < p.q.size(); ++i) q.push_back(p.q[i]);
         return *this;
     }
+    /**
+     * Override '<' operator for next_permutation
+     */
     bool operator <(const FactoryPartGQ &p) const {
         return id < p.id;
     }
@@ -226,28 +237,28 @@ struct Factory
     }
 };
 
-struct Item
+struct PossibleCase
 {
-    long double x;
+    long double probability;
     vector<int> g,d;
     int c,reliability;
-    Item()
+    PossibleCase()
     {
-        x=1;
+        probability=1;
         c=INF;
         reliability=INF;
         g.clear();
         d.clear();
     }
-    Item(long double x):x(x)
+    PossibleCase(long double probability):probability(probability)
     {
         c=INF;
         reliability=INF;
         g.clear();
         d.clear();
     }
-    const Item& operator =(const Item& p) {
-        x=p.x;
+    const PossibleCase& operator =(const PossibleCase& p) {
+        probability=p.probability;
         c=p.c;
         reliability=p.reliability;
         g.clear();
@@ -256,21 +267,22 @@ struct Item
         for (int i = 0; i < p.d.size(); ++i) d.push_back(p.d[i]);
         return *this;
     }
-    const Item& operator *=(const Item& p)
+    /**
+     * Override '*=' to '(x)'
+     */
+    const PossibleCase& operator *=(const PossibleCase& p)
     {
-        x*=p.x;
+        probability *= p.probability;
         c=max(c,p.c);
         for (int i = 0; i < p.g.size(); ++i) g.push_back(p.g[i]);
         for (int i = 0; i < p.d.size(); ++i) d.push_back(p.d[i]);
         return *this;
     }
-    long double get_reliability(int* w, int h)
+    /**
+     * For each case, get its reliability
+     */
+    long double get_reliability(int* w, int rt)
     {
-        // if (c!=4 && c!=0)
-        // {
-        //     cout<<(*this)<<endl;
-        // }
-        // if (h>=0) return 0;
         reliability = 0;
         for (int i = 0; i < g.size(); ++i)
         {
@@ -279,18 +291,19 @@ struct Item
                 reliability += (g[i]-d[i])*w[i];
             }
         }
-        if (reliability < h)
+        if (reliability < rt)
         {
-            // cout<<*this<<","<<reliability<<endl;
-            return x;
+            return probability;
         }
         return 0;
     }
-    void transfer_for_sr(int* w, int h)
+    /**
+     * For each case, get the preliminary optimized plan
+     */
+    void transfer_for_sr(int* w, int rt)
     {
-        if (cmp(this->get_reliability(w,h),0) && c>0)
+        if (cmp(this->get_reliability(w,rt),0) && c>0)
         {
-            // cout<<(*this)<<":"<<this->get_reliability(w,h)<<" - ";
             int l = 0;
             int r = g.size()-1;
             while (l<g.size() && r>=0 && c>0)
@@ -306,17 +319,17 @@ struct Item
                     c-=delta;
                 }
             }
-            // this->get_reliability(w,h);
-            // cout<<(*this)<<":"<<this->get_reliability(w,h)<<endl;
         }
     }
-    void transfer_for_mr(int* w, int h)
+    /**
+     * For each case, get the final optimized plan
+     */
+    void transfer_for_mr(int* w, int rt)
     {
         int l = 0;
         int r = g.size()-1;
-        if (cmp(this->get_reliability(w,h),0) && c>0)
+        if (cmp(this->get_reliability(w,rt),0) && c>0)
         {
-            // cout<<(*this)<<":"<<this->get_reliability(w,h)<<" - ";
             while (l<r && c>0)
             {
                 while (l<r && g[l]-d[l]>=0) l++;
@@ -330,13 +343,11 @@ struct Item
                     c-=delta;
                 }
             }
-            // this->get_reliability(w,h);
-            // cout<<(*this)<<":"<<this->get_reliability(w,h)<<endl;
         }
     }
-    friend ostream& operator <<(ostream &os, const Item &p) 
+    friend ostream& operator <<(ostream &os, const PossibleCase &p) 
     {
-        os<<p.x<<"[";
+        os<<p.probability<<"[";
         if (p.g.size()>0)
         {
             os<<"{";
@@ -377,17 +388,16 @@ struct Item
 };
 
 int i,j,k,n,m,x,y,l,r,tcase,xcase;
-int ta,min_h;
-double td;
+int rt;
 double result_r,result_sr,result_mr;
 int number_of_factory,size_conveyor;
 int size_g_q[MAXN],size_d_s[MAXN];
-vector<Item> ua,ugf_r,ugf_sr,ugf_mr;
+vector<PossibleCase> ua,ugf_r,ugf_sr,ugf_mr;
 int weight[MAXN],c[MAXM];
 double beta[MAXM];
 vector<int> g[MAXN],d[MAXN];
 vector<double> q[MAXN],s[MAXN];
-vector<Item> u[MAXN],w[MAXN],delta[MAXN],ugf[MAXN];
+vector<PossibleCase> u[MAXN],w[MAXN],delta[MAXN],ugf[MAXN];
 Factory f[MAXN];
 FactoryPartGQ fpq[MAXN];
 
@@ -413,7 +423,7 @@ FactoryPartGQ init(Factory& p)
 
 void inputs()
 {
-    cin>>min_h;
+    cin>>rt;
     cin>>number_of_factory;
     for (int i = 0; i < number_of_factory; ++i)
     {
@@ -424,7 +434,6 @@ void inputs()
         cin>>f[i];
         f[i].id = i;
         fpq[i]=init(f[i]);
-        // cout<<f[i];
     }
     cin>>size_conveyor;
     for (int i = 0; i < size_conveyor; ++i)
@@ -457,22 +466,22 @@ void prepare_data()
         {
             g[i].push_back(f[i].g[j]);
             q[i].push_back(f[i].q[j]);
-            u[i].push_back(Item(f[i].q[j]));
+            u[i].push_back(PossibleCase(f[i].q[j]));
             u[i][j].g.push_back(f[i].g[j]);
         }
         for (int j = 0; j < f[i].d.size(); ++j)
         {
             d[i].push_back(f[i].d[j]);
             s[i].push_back(f[i].s[j]);
-            w[i].push_back(Item(f[i].s[j]));
+            w[i].push_back(PossibleCase(f[i].s[j]));
             w[i][j].d.push_back(f[i].d[j]);
         }
     }
 }
 
-void get_inputs()
+void output_original_data()
 {
-    cout<<"Minimum H: "<<min_h<<endl;
+    cout<<"H: "<<rt<<endl;
     cout << number_of_factory << " factories." << endl;
     cout << "W: ";
     for (int i = 0; i < number_of_factory; ++i)
@@ -524,170 +533,117 @@ void get_inputs()
     cout<<endl;
 }
 
-vector<Item> calculate_multiply(vector<Item> a, vector<Item> b)
+/**
+ * Calculate a (x) b
+ */
+vector<PossibleCase> calculate_multiply(vector<PossibleCase> a, vector<PossibleCase> b)
 {
-    vector<Item> result;
+    vector<PossibleCase> result;
     for (int i = 0; i <a.size(); ++i)
     {
         for (int j = 0; j < b.size(); ++j)
         {
-            Item item = a[i];
-            item *= b[j];
-            result.push_back(item);
+            PossibleCase pc = a[i];
+            pc *= b[j];
+            result.push_back(pc);
         }
     }
-    // cout<<"result: ";
-    // for (int i = 0; i < result.size(); ++i)
-    // {
-    //     cout<<result[i]<<",";
-    // }
-    // cout<<endl;
     return result;
 }
 
-vector<Item> calculate_multiply(vector<Item> a, int* c, double* beta, int size_conveyor)
+/**
+ * Calculate a (x) beta (beta is a list of conveyor)
+ */
+vector<PossibleCase> calculate_multiply(vector<PossibleCase> a, int* c, double* beta, int size_conveyor)
 {
-    vector<Item> result;
+    vector<PossibleCase> result;
     for (int i = 0; i <a.size(); ++i)
     {
         for (int j = 0; j < size_conveyor; ++j)
         {
-            Item item = a[i];
-            item.x *= beta[j];
-            item.c = c[j];
-            result.push_back(item);
+            PossibleCase pc = a[i];
+            pc.probability *= beta[j];
+            pc.c = c[j];
+            result.push_back(pc);
         }
     }
-    // cout<<"result: ";
-    // for (int i = 0; i < result.size(); ++i)
-    // {
-    //     cout<<result[i]<<",";
-    // }
-    // cout<<endl;
     return result;
 }
 
-double calculat_r(vector<Item> u, int* weight, int h)
+/**
+ * Calculate R for case u with weight and rt
+ * vector<PossibleCase> u contains all the possible cases with their final plan
+ */
+double calculat_r(vector<PossibleCase> u, int* weight, int rt)
 {
-    // cout<<"u: ";
-    // for (int i = 0; i < u.size(); ++i)
-    // {
-    //     cout<<u[i]<<",";
-    // }
-    // cout<<endl;
     double result = 0;
     for (int i = 0; i < u.size(); ++i)
     {
-        result+=u[i].get_reliability(weight,h);
+        result+=u[i].get_reliability(weight,rt);
     }
-    // cout<<"r: "<<setprecision(10)<<result<<endl;
     result = 1 - result;
     return result;
 }
 
 int main()
 {
-    tcase = 1;
     freopen("s.in","r",stdin);
     freopen("s.out","w",stdout);
-    // #ifdef Smile
-    //     freopen("s.in","r",stdin);
-    //     freopen("s.out","w",stdout);
-    //     cin>>tcase;
-    // #endif
+
     cout<<"========================= "<< currentDateTime() <<" =========================="<<endl;
 
+    inputs();
+    prepare_data();
+    output_original_data();
+    cout<<endl;
+    cout<<"#\tPermutation\tR\t\t\t\tSR\t\t\t\tMR"<<endl;
     xcase=0;
-    while (xcase++ < tcase)
+    
+    do
     {
-        inputs();
         prepare_data();
-        get_inputs();
-        cout<<endl;
-        cout<<"#\tH\tR\tSR\tMR"<<endl;
-
+        // output_original_data();
         for (int i = 0; i < number_of_factory; ++i)
         {
             delta[i] = calculate_multiply(u[i],w[i]);
         }
-        ugf[0].push_back(Item(1));
+        ugf[0].push_back(PossibleCase(1));
         for (int i = 1; i <= number_of_factory; ++i)
         {
             ugf[i] = calculate_multiply(ugf[i-1],delta[i-1]);
         }
-        ugf_r=calculate_multiply(ugf[number_of_factory],c,beta,size_conveyor);
-        for (int h = min_h; h <= 0; ++h)
-        {
-            result_r = calculat_r(ugf_r,weight,h);
-            // cout<<"R: "<<result_r<<endl;
-            ugf_sr=ugf_r;
-            for (int i = 0; i < ugf_sr.size(); ++i)
-            {
-                ugf_sr[i].transfer_for_sr(weight,h);
-            }
-            result_sr = calculat_r(ugf_sr,weight,h);
-            // cout<<"SR: "<<result_sr<<endl;
-            ugf_mr=ugf_sr;
-            for (int i = 0; i < ugf_mr.size(); ++i)
-            {
-                ugf_mr[i].transfer_for_mr(weight,h);
-            }
-            result_mr = calculat_r(ugf_mr,weight,h);
-            // cout<<"MR: "<<result_mr<<endl;
-            // get_inputs();
-            // cout << "Case #" << xcase++ << ": " << endl;
-            // cout<<"H: "<<h<<endl;
-            // cout<<"R: "<<setprecision(12)<<result_r<<endl;
-            // cout<<"SR: "<<setprecision(12)<<result_sr<<endl;
-            // cout<<"MR: "<<setprecision(12)<<result_mr<<endl;
-            cout<<setprecision(12)<<xcase++<<"\t"<<h<<"\t"<<result_r<<"\t"<<result_sr<<"\t"<<result_mr<<endl;
-        }
 
-        cout<<"#\tW\tR\tSR\tMR"<<endl;
-        xcase=0;
-        
-        do
+        // Calculate the initial R
+        ugf_r=calculate_multiply(ugf[number_of_factory],c,beta,size_conveyor);
+        result_r = calculat_r(ugf_r,weight,rt);
+
+        // Get the preliminary optimized plan and calculate SR
+        ugf_sr=ugf_r;
+        for (int i = 0; i < ugf_sr.size(); ++i)
         {
-            prepare_data();
-            // get_inputs();
-            for (int i = 0; i < number_of_factory; ++i)
-            {
-                delta[i] = calculate_multiply(u[i],w[i]);
-            }
-            ugf[0].push_back(Item(1));
-            for (int i = 1; i <= number_of_factory; ++i)
-            {
-                ugf[i] = calculate_multiply(ugf[i-1],delta[i-1]);
-            }
-            ugf_r=calculate_multiply(ugf[number_of_factory],c,beta,size_conveyor);
-            int h=-5;
-            result_r = calculat_r(ugf_r,weight,h);
-            ugf_sr=ugf_r;
-            for (int i = 0; i < ugf_sr.size(); ++i)
-            {
-                ugf_sr[i].transfer_for_sr(weight,h);
-            }
-            result_sr = calculat_r(ugf_sr,weight,h);
-            ugf_mr=ugf_sr;
-            for (int i = 0; i < ugf_mr.size(); ++i)
-            {
-                ugf_mr[i].transfer_for_mr(weight,h);
-            }
-            result_mr = calculat_r(ugf_mr,weight,h);
-            cout<<setprecision(12)<<xcase++<<"\t";
-            for (int i = 0; i < number_of_factory; ++i)
-            {
-                cout<<f[i].id+1;
-            }
-            cout<<"\t"<<result_r<<"\t"<<result_sr<<"\t"<<result_mr<<endl;
+            ugf_sr[i].transfer_for_sr(weight,rt);
         }
-        while (next_permutation(fpq,fpq+number_of_factory));
+        result_sr = calculat_r(ugf_sr,weight,rt);
+
+        // Get the final optimized plan and calculate MR
+        ugf_mr=ugf_sr;
+        for (int i = 0; i < ugf_mr.size(); ++i)
+        {
+            ugf_mr[i].transfer_for_mr(weight,rt);
+        }
+        result_mr = calculat_r(ugf_mr,weight,rt);
+
+        // Output the result for this permutation
+        cout<<setprecision(10)<<fixed<<xcase++<<"\t";
+        for (int i = 0; i < number_of_factory; ++i)
+        {
+            cout<<f[i].id+1;
+        }
+        cout<<"\t\t"<<result_r<<"\t"<<result_sr<<"\t"<<result_mr<<endl;
     }
+    while (next_permutation(fpq,fpq+number_of_factory)); // next_permutation enumerate the full permutation of the factories.
 
     cout<<"========================= "<< currentDateTime() <<" =========================="<<endl;
 
-    #ifndef Smile
-        system("pause");
-    #endif
     return 0;
 }
